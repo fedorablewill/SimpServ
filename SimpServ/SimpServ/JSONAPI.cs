6e48b7e7a9f91d1daac50f0b491bf216a5de96cb
Will Stephenson <absorr@live.com>
2013-07-05 19:51:48 -0400

    Finished JSONAPI communicator, finished login
    TODO: The login stores the data for future use, but I have yet to have
    it encode that data. Also, I want it to authenticate the server before
    moving on to the next page.

--- SimpServ/SimpServ/JSONAPI.cs
+++ SimpServ/SimpServ/JSONAPI.cs
@@ -6,9 +6,21 @@ using System.Threading.Tasks;
 using System.Collections;
 using System.IO;
 using System.Net;
+<<<<<<< HEAD
 using Windows.Data.Json;
 
 namespace com.nuclearpowered.SimpServ
+=======
+using System.Net.Http;
+using Windows.Data.Json;
+using Windows.Security.Cryptography.Core;
+using Windows.Security.Cryptography;
+using Windows.UI.Popups;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace SimpServ
+>>>>>>> Created graphics, login splash, and communicator
 {
     class JSONAPI
     {
@@ -19,7 +31,11 @@ namespace com.nuclearpowered.SimpServ
         private string salt;
 
         private string urlFormat_call = "http://{0}:{1}/api/call?method={2}&args={3}&key={4}";
+<<<<<<< HEAD
         private string urlFormat_callMultiple = "http:/{0}:{1}/api/call-multiple?method={2}&args={3}&key={4}";
+=======
+        //private string urlFormat_callMultiple = "http:/{0}:{1}/api/call-multiple?method={2}&args={3}&key={4}";
+>>>>>>> Created graphics, login splash, and communicator
 
         public JSONAPI(string par1, int par2, string par3, string par4, string par5)
         {
@@ -30,12 +46,41 @@ namespace com.nuclearpowered.SimpServ
             this.salt = par5;
         }
 
+<<<<<<< HEAD
         public JsonValue call(string method, Array args)
         {
             if (args != null)
             {
                 string sArgs = ArrayToJSON(args);
             }
+=======
+        public JsonValue call(string method, string[] args)
+        {
+            string sArgs;
+            if (args != null)
+                sArgs = ArrayToJSON(args);
+            else
+                sArgs = "[]";
+
+            string raw = this.username + method + this.password + this.salt;
+            IBuffer input = CryptographicBuffer.ConvertStringToBinary(raw, BinaryStringEncoding.Utf8);
+
+            // hash it...
+            var hasher = HashAlgorithmProvider.OpenAlgorithm("SHA256");
+            IBuffer hashed = hasher.HashData(input);
+
+            // format it...
+            string key = CryptographicBuffer.EncodeToHexString(hashed);
+            MessageDialog msg = new MessageDialog(key + " is key for " + raw);
+            IAsyncOperation<IUICommand> x = msg.ShowAsync();
+
+            HttpClient client = new HttpClient();
+            Task<HttpResponseMessage> resp = client.GetAsync(Uri.EscapeUriString(String.Format(urlFormat_call, host, port, Uri.EscapeUriString(method), Uri.EscapeUriString(sArgs), Uri.EscapeUriString(key))));
+
+            JsonValue json = JsonValue.Parse(resp.Result.Content.ReadAsStringAsync().Result);
+
+            return json;
+>>>>>>> Created graphics, login splash, and communicator
         }
 
         private string ArrayToJSON(Array par1)
--- SimpServ/SimpServ/MainPage.xaml.cs
+++ SimpServ/SimpServ/MainPage.xaml.cs
@@ -11,6 +11,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+<<<<<<< HEAD
+=======
+using Windows.Data.Json;
+using Windows.Storage;
+>>>>>>> Created graphics, login splash, and communicator
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,6 +26,12 @@ namespace SimpServ
     /// </summary>
     public sealed partial class MainPage : Page
     {
+<<<<<<< HEAD
+=======
+
+        private StorageFile fData;
+
+>>>>>>> Created graphics, login splash, and communicator
         public MainPage()
         {
             this.InitializeComponent();
@@ -31,8 +42,46 @@ namespace SimpServ
         /// </summary>
         /// <param name="e">Event data that describes how this page was reached.  The Parameter
         /// property is typically used to configure the page.</param>
+<<<<<<< HEAD
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+=======
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var var1 = ApplicationData.Current.LocalFolder;
+            this.fData = await var1.GetFileAsync("login.dat");
+            try
+            {
+                var da = await FileIO.ReadLinesAsync(this.fData);
+                Array data = da.ToArray();
+                foreach (string dat in data)
+                {
+                    string[] sp = dat.Split('~');
+                    if (sp[0] == "ip")
+                        this.txtIP.Text = sp[1];
+                    else if (sp[0] == "port")
+                        this.txtPort.Text = sp[1];
+                    else if (sp[0] == "user")
+                        this.txtUser.Text = sp[1];
+                    else if (sp[0] == "pass")
+                        this.txtPass.Password = sp[1];
+                    else if (sp[0] == "salt")
+                        this.txtSalt.Password = sp[1];
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                this.fData = ApplicationData.Current.LocalFolder.CreateFileAsync("login.dat").GetResults();
+            }
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            await FileIO.WriteLinesAsync(this.fData, new string[] { "ip~" + this.txtIP.Text, 
+                "port~" + this.txtPort.Text, "user~" + this.txtUser.Text, "pass~" + this.txtPass.Password, 
+                "salt~" + this.txtSalt.Password }, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+            this.Frame.Navigate(typeof(Dashboard), new JSONAPI(this.txtIP.Text, Convert.ToInt32(this.txtPort.Text), this.txtUser.Text, this.txtPass.Password, this.txtSalt.Password));
+>>>>>>> Created graphics, login splash, and communicator
         }
     }
 }
